6dab3f0aeb46c5703907044f82fb4122ca599ea2
Daniel Alejandro <dnlalejandro1@gmail.com>
2022-08-17 23:32:19 -0500

    agregacion de cierres y aperturas de menus

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', toggleDesktopMenu);
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
-function toggleDesktopMenu(){
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMenuCarritoClosed = menuCarritoIcon.classList.contains('inactive');
+
+    if (!isMobileMenuClosed || !isMenuCarritoClosed ) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
 }
